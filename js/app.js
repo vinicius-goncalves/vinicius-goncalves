9ddbe895445ed36a6f030a83643e882214fa9f6e
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-06-23 12:34:34 -0300

    Fixed spelling errors

--- js/app.js
+++ js/app.js
@@ -14,5 +14,16 @@ window.onload = () => {
     setTimeout(() => {
         expandMoreIcon.classList.add('active-expand_more--icon')
     }, 1000)
-    
 }
+
+const randomCharacters = (length) => {
+    let result = ""
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    const charactersLength = characters.length
+    for(let i = 0; i < length; i++) {
+        result += characters.charAt(Math.random() * charactersLength)
+    }
+    return result
+}
+
+console.log(randomCharacters(6))
