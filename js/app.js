d45b7d0076c93be4317a0096a0cdb0c24832cc1e
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-06-23 18:47:10 -0300

    Creating some sections, navbar created and more

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const firstWord = document.querySelector('.first-word')
 const firstLore = document.querySelector('.first-lore')
 const expandMoreIcon = document.querySelector('.expand_more--icon')
+const spelling = document.querySelector('.spelling')
+
+const navbar = document.querySelector('nav')
+const main = document.querySelector('main')
 
 window.onload = () => {
     setTimeout(() => {
@@ -13,17 +17,52 @@ window.onload = () => {
 
     setTimeout(() => {
         expandMoreIcon.classList.add('active-expand_more--icon')
+        document.querySelector('.alert').classList.add('active-alert')
+
+    }, 1000)
+
+    setTimeout(() => {
+        spelling.classList.add('active-spelling-effect')
+    }, 900)
+
+    setTimeout(() => {
+        navbar.classList.add('active-navbar')
     }, 1000)
 }
 
-const randomCharacters = (length) => {
-    let result = ""
-    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    const charactersLength = characters.length
-    for(let i = 0; i < length; i++) {
-        result += characters.charAt(Math.random() * charactersLength)
-    }
-    return result
-}
+const x = Array.from(navbar.children)
+const documentEl = document.documentElement
 
-console.log(randomCharacters(6))
+
+window.addEventListener('scroll', () => {
+    const clientHeightDynamic = documentEl.clientHeight - documentEl.scrollTop
+    if(documentEl.scrollTop === 0) {
+        navbar.classList.remove('active-navbar-color')
+    }
+    
+    if(clientHeightDynamic <= 880) {
+        navbar.classList.add('active-navbar-color')
+    }
+    
+    if(clientHeightDynamic <= 680) {
+        main.classList.add('active-main')
+    }
+
+    if(clientHeightDynamic <= 890) {
+        document.querySelector('.alert').classList.add('alert-remove')
+    }else {
+        document.querySelector('.alert').classList.remove('alert-remove')
+    }
+})
+
+// const randomCharacters = (length) => {
+//     let result = ""
+//     const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+//     const charactersLength = characters.length
+//     for(let i = 0; i < length; i++) {
+//         result += characters.charAt(Math.random() * charactersLength)
+//     }
+//     return result
+// }
+
+// console.log(randomCharacters(6))
