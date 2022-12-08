575f17f8241cb47ee9bd74e91e50a97b89942dd0
Vinicius Goncalves <oldz.registros@gmail.com>
2022-12-08 16:42:20 -0300

    Removed some css classes. Done some code refactoring

--- js/app.js
+++ js/app.js
@@ -1,72 +1,68 @@
-const firstWord = document.querySelector('.first-word')
-const firstLore = document.querySelector('.first-lore')
-const expandMoreIcon = document.querySelector('.expand_more--icon')
-const spelling = document.querySelector('.spelling')
-
-const navbar = document.querySelector('nav')
 const main = document.querySelector('main')
+const navbar = document.querySelector('nav')
+const docEl = document.documentElement
 
-window.onload = () => {
+function firstInitialization() {
+
+    const firstWord = document.querySelector('.first-word')
+    const firstLore = document.querySelector('.first-lore')
+    const expandMoreIcon = document.querySelector('.expand_more--icon')
+    const links = document.querySelector('.a_links')
+    
     setTimeout(() => {
-        firstWord.classList.add('active-first-word')
+        firstWord.classList.add('active-x-opacity-effect')
     }, 500)
     
     setTimeout(() => {
-        firstLore.classList.add('active-first-lore')
+        firstLore.classList.add('active-x-opacity-effect')
     }, 600)
 
     setTimeout(() => {
         expandMoreIcon.classList.add('active-expand_more--icon')
         document.querySelector('.alert').classList.add('active-alert')
-
     }, 1000)
 
     setTimeout(() => {
-        spelling.classList.add('active-spelling-effect')
+        links.classList.add('active-a_links-effect')
     }, 900)
 
     setTimeout(() => {
         navbar.classList.add('active-navbar')
     }, 1000)
+
 }
 
-const x = Array.from(navbar.children)
-const documentEl = document.documentElement
+window.addEventListener('DOMContentLoaded', firstInitialization)
 
+const principalSection = document.querySelector('.principal-section')
 
 window.addEventListener('scroll', () => {
-    const clientHeightDynamic = documentEl.clientHeight - documentEl.scrollTop
-    if(documentEl.scrollTop === 0) {
-        navbar.classList.remove('active-navbar-color')
-    }
-    
-    if(clientHeightDynamic <= 880) {
-        navbar.classList.add('active-navbar-color')
-    }
-    
-    if(clientHeightDynamic <= 680) {
-        main.classList.add('active-main')
-        const skillsChildren = document.querySelector('.skills')
-        setTimeout(() => {
-            skillsChildren.querySelectorAll('.spelling').forEach(child => child.classList.add('active-spelling-effect'))
-        }, 1000 / 2)
+
+    const principalSectionRect = principalSection.getBoundingClientRect()
+
+    if(principalSectionRect.bottom <= 0) {
+        main.classList.add('class', 'active-main')
     }
 
-    if(clientHeightDynamic <= 890) {
-        document.querySelector('.alert').classList.add('alert-remove')
-    }else {
-        document.querySelector('.alert').classList.remove('alert-remove')
-    }
+    // if(docEl.scrollTop === 0) {
+    //     navbar.classList.remove('active-navbar-color')
+    // }
+    
+    // if(clientHeightDynamic <= 880) {
+    //     navbar.classList.add('active-navbar-color')
+    // }
+    
+    // if(clientHeightDynamic <= 680) {
+    //     main.classList.add('active-main')
+    //     const skillsChildren = document.querySelector('.skills')
+    //     setTimeout(() => {
+    //         skillsChildren.querySelectorAll('.a_links').forEach(child => child.classList.add('active-a_links-effect'))
+    //     }, 1000 / 2)
+    // }
+
+    // if(clientHeightDynamic <= 890) {
+    //     document.querySelector('.alert').classList.add('alert-remove')
+    // }else {
+    //     document.querySelector('.alert').classList.remove('alert-remove')
+    // }
 })
-
-// const randomCharacters = (length) => {
-//     let result = ""
-//     const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-//     const charactersLength = characters.length
-//     for(let i = 0; i < length; i++) {
-//         result += characters.charAt(Math.random() * charactersLength)
-//     }
-//     return result
-// }
-
-// console.log(randomCharacters(6))
