1f86d79221109d1b81df0ec478f6b7d74ed96b4d
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-03-05 10:55:21 -0300

    feat: added meta tags for better readability and added links tags for better performance; refactor: other small fixies

--- src/utils/update-title.ts
+++ src/utils/update-title.ts
@@ -1,6 +1,6 @@
 function updateTitle(newTitle: string): void {
     const capitalizedText = newTitle[0].toUpperCase() + newTitle.slice(1);
-    document.title =capitalizedText + ' | vinicius-goncalves.com';
+    document.title = capitalizedText + ' | vinicius-goncalves.com';
 }
 
 export default updateTitle;
