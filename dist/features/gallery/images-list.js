ddb0f6c01ba3b74c1ea39855747209f82826805a
Vinícius Gonçalves <oldz.registros@gmail.com>
2024-03-14 09:39:28 -0300

    added new project to projects gallery

--- dist/features/gallery/images-list.js
+++ dist/features/gallery/images-list.js
@@ -1,8 +1,9 @@
 const imagesSrc = [
-    { title: 'Estrutura de pastas', src: 'assets/images/gallery/image-1.jpg' },
-    { title: 'Sistema de carrinho', src: 'assets/images/gallery/image-2.jpg' },
-    { title: 'Paginação', src: 'assets/images/gallery/image-3.jpg' },
-    { title: 'Modals vanilla', src: 'assets/images/gallery/image-4.jpg' }
+    { title: 'Pokedex', src: 'assets/images/gallery/image-1.jpg' },
+    { title: 'Estrutura de pastas', src: 'assets/images/gallery/image-2.jpg' },
+    { title: 'Sistema de carrinho', src: 'assets/images/gallery/image-3.jpg' },
+    { title: 'Modals vanilla', src: 'assets/images/gallery/image-4.jpg' },
+    { title: 'Paginação', src: 'assets/images/gallery/image-5.jpg' },
 ];
 const MAX_IMAGES_LENGTH = imagesSrc.length;
 export { imagesSrc, MAX_IMAGES_LENGTH };
--- src/features/gallery/images-list.ts
+++ src/features/gallery/images-list.ts
@@ -1,10 +1,11 @@
 type GalleryImage = { title: string, src: string };
 
 const imagesSrc: Array<GalleryImage> = [
-    { title: 'Estrutura de pastas', src: 'assets/images/gallery/image-1.jpg' },
-    { title: 'Sistema de carrinho', src: 'assets/images/gallery/image-2.jpg' },
-    { title: 'Paginação', src: 'assets/images/gallery/image-3.jpg' },
-    { title: 'Modals vanilla', src: 'assets/images/gallery/image-4.jpg' }
+    { title: 'Pokedex', src: 'assets/images/gallery/image-1.jpg' },
+    { title: 'Estrutura de pastas', src: 'assets/images/gallery/image-2.jpg' },
+    { title: 'Sistema de carrinho', src: 'assets/images/gallery/image-3.jpg' },
+    { title: 'Modals vanilla', src: 'assets/images/gallery/image-4.jpg' },
+    { title: 'Paginação', src: 'assets/images/gallery/image-5.jpg' },
 ];
 
 const MAX_IMAGES_LENGTH: number = imagesSrc.length;
